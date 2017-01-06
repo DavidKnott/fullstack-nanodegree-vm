f623a6270d15fe23f338d4fbee50759177f6a413
David Knott <david1k1nott@gmail.com>
2017-01-06 10:42:34 -0700

    add in readme

--- README.md
+++ README.md
@@ -5,16 +5,18 @@ Common code for the Relational Databases and Full Stack Fundamentals courses
 
 To run the tournament project you need to:
 
-- clone my github repo
+- type "git clone https://github.com/DavidKnott/fullstack-nanodegree-vm.git" into your terminal
 
-- cd fullstack/vagrant
+- enter "cd fullstack-nanodegree-vm/vagrant"
 
-- vagrant up (you can turn off the VM with 'vagrant halt')
+- "vagrant up" (you can turn off the VM with 'vagrant halt')
 
-- vagrant ssh (from here you can type 'exit' to log out)
+- "vagrant ssh" (from here you can type 'exit' to log out)
 
 - cd /vagrant/tournament
 
 - psql -f tournament.sql
 
-- python tournament_results.py
+- python tournament_test.py
+
+- if it works correctly you should see "Success!  All tests pass!"
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,14 +1,21 @@
+-- Clear out any previous DB.
+DROP DATABASE IF EXISTS tournament;
+
+-- Create DB.
+CREATE DATABASE tournament;
+
+-- Connect to the DB before creating tables.
+\c tournament;
+
+-- Creates sql table for players with the attributes:
+-- id: which is the way to keep track of them
+-- name: which is each players name
+-- wins: which is the number of wins each player has
+-- matches: which is the number of matches each player has played
+
 CREATE TABLE players (id SERIAL PRIMARY KEY,
                       name TEXT,
                       wins integer DEFAULT (0),
                       matches integer DEFAULT (0),
                       time TIMESTAMP DEFAULT CURRENT_TIMESTAMP );
 
-"""
-  Creates sql table for players with the attributes:
-  id: which is the way to keep track of them
-  name: which is each players name
-  wins: which is the number of wins each player has
-  matches: which is the number of matches each player has played
-
-"""
